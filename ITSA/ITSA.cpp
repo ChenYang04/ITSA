d8ff09ed8d7d9da4651f2714573d585dc707ecc5
User <User@MSI>
2023-03-07 17:14:16 +0800

    ITSA_04 停車費計算

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int a, b;
 	int c, d;
-	cin >> a >> c ;
+	cin >> a >> b >> c >> d;
 	int i,j;
 	int x, y;
 	int z;
@@ -13,83 +13,83 @@ int main()
 	switch (a)
 	{
 	case 13:
-		i = 1 * 60;
+		i = 1;
 		break;
 	case 14:
-		i = 2 * 60;
+		i = 2;
 		break;
 	case 15:
-		i = 3 * 60;
+		i = 3 ;
 		break;
 	case 16:
-		i = 4 * 60;
+		i = 4;
 		break;
 	case 17:
-		i = 5 * 60;
+		i = 5 ;
 		break;
 	case 18:
-		i = 6 * 60;
+		i = 6 ;
 		break;
 	case 19:
-		i = 7 * 60;
+		i = 7 ;
 		break;
 	case 20:
-		i = 8 * 60;
+		i = 8 ;
 		break;
 	case 21:
-		i = 9 * 60;
+		i = 9;
 		break;
 	case 22:
-		i = 10 * 60;
+		i = 10;
 		break;
 	case 23:
-		i = 11 * 60;
+		i = 11;
 		break;
 	case 24:
-		i = 12 * 60;
+		i = 12;
 		break;
 	}
 	switch (c)
 	{
 	case 13:
-		j = 1 * 60;
+		j = 1;
 		break;
 	case 14:
-		j= 2 * 60;
+		j = 2;
 		break;
 	case 15:
-		j= 3 * 60;
+		j = 3;
 		break;
 	case 16:
-		j = 4 * 60;
+		j = 4;
 		break;
 	case 17:
-		j = 5 * 60;
+		j = 5;
 		break;
 	case 18:
-		j = 6 * 60;
+		j = 6;
 		break;
 	case 19:
-		j = 7 * 60;
+		j = 7;
 		break;
 	case 20:
-		j = 8 * 60;
+		j = 8;
 		break;
 	case 21:
-		j = 9 * 60;
+		j = 9;
 		break;
 	case 22:
-		j = 10 * 60;
+		j = 10;
 		break;
 	case 23:
-		j = 11 * 60;
+		j = 11;
 		break;
 	case 24:
-		j = 12 * 60;
+		j = 12;
 		break;
 	}
 
-	z = i + j;
-	cout << z <<endl;
+	z = (i + j);
+
 }
 
