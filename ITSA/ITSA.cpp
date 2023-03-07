cbc37d60c9a379c7ed0c999bb1f37e844f6b2835
User <User@MSI>
2023-03-07 18:29:10 +0800

    ITSA_05 十進制轉二進制

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,8 +1,19 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World";
+    int num;
+    cout << "請輸入一個整數 (-128~127): ";
+    cin >> num;
+
+
+    for (int i = 7; i >= 0; i--)
+    {
+        int bit = (num >> i) & 1;
+        cout << bit;
+    }
+
+    cout << endl;
+    return 0;
 }
-
-
