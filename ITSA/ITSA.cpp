9ce359d8a49581bb50952205dc3a431f2e8a8e63
User <User@MSI>
2023-03-07 18:14:46 +0800

    ITSA

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,7 +1,44 @@
 ﻿#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int num;
+    cout << "請輸入一個整數 (-128~127): ";
+    cin >> num;
+
+    // 利用位元運算子轉換成二進制表示
+    for (int i = 7; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+        cout << bit;
+    }
+
+    cout << endl;
+    return 0;
 }
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int a[10];
+//	int b, c;
+//	int z;
+//	cin >> c;
+//	
+//	cout << "輸入一個整數，介於-128～127之間 = ";
+//
+//	for (b = 0; c > 0; b++)
+//	{
+//		a[b] = c % 2;
+//		c = c / 2;
+//	}
+//	for (b = b - 1; b >= 0; b--)
+//	{
+//		 cout << setw(8) << setfill('0') <<a[b] << endl;
+//	}
+//	
+//	
+//}
 
