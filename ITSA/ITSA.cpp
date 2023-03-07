180c392cda59ac8407d89425d4a0845962ea9840
User <User@MSI>
2023-03-07 18:38:49 +0800

    ITSA_06 季節判定

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,8 +1,53 @@
 ﻿#include <iostream>
-
+using namespace std;
 int main()
 {
-	std::cout << "Hello World";
+	int s;
+	cout << "輸入月份 = ";
+	cin >> s;
+
+	
+
+	switch (s)
+	{
+	case 3:
+		cout << "春季";
+		break;
+	case 4:
+		cout << "春季";
+		break;
+	case 5:
+		cout << "春季";
+		break;
+	case 6:
+		cout << "夏季";
+		break;
+	case 7:
+		cout << "夏季";
+		break;
+	case 8:
+		cout << "夏季";
+		break;
+	case 9:
+		cout << "秋季";
+		break;
+	case 10:
+		cout << "秋季";
+		break;
+	case 11:
+		cout << "秋季";
+		break;
+	case 12:
+		cout << "冬季";
+		break;
+	case 1:
+		cout << "冬季";
+		break;
+	case 2:
+		cout << "冬季";
+		break;
+	}
+	
 }
 
 
