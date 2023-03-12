b2c4f647b5f22c1c3bc7514948531271483438c1
User <User@MSI>
2023-03-12 23:54:25 +0800

    ITSA_03 判斷座標是否在圓形的範圍內

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,14 +1,25 @@
 ﻿#include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
+int main() {
+    int center_x = 0;
+    int center_y = 0;
+
+    int radius = 100;
+
     int x, y;
     cin >> x >> y;
-    if ((x <= 100) | (y <= 100))
-    {
-        cout << "inside"<<endl;
-    }
-    else cout << "outsite"<<endl;
-}
 
+    double distance = sqrt(pow(x - center_x, 2) + pow(y - center_y, 2));
+
+    if (distance <= radius) {
+        cout << "inside" << endl;
+    }
+    else {
+        cout << "outside" << endl;
+    }
+
+    return 0;
+}
