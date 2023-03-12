4e3f0f52ea98bb3fbf8cd901e073bd047b783ff3
User <User@MSI>
2023-03-12 23:08:58 +0800

    ITSA_03 判斷座標是否在圓形的範圍內

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main()
 {
-    float x, y;
-
+    int x, y;
     cin >> x >> y;
-
     if ((x <= 100) | (y <= 100))
     {
-        cout << "inside";
+        cout << "inside"<<endl;
     }
-    else cout << "outsite";
+    else cout << "outsite"<<endl;
 }
 
