7a74daa6924abdfaf4ed16b26252221b63e32a58
User <User@MSI>
2023-03-16 18:31:44 +0800

    ITSA_06 質數判別

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main()
 {
-	cout >> "hello world" >> endl;
+	int n;
+	cin >> n;
+	int pn =0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			pn++;
+		}
+	}
+	if (pn == 2)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+	
+
 
 }
 
