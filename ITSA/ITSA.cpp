265b301b6d00cf15531390c2a4da7082f2bf2a82
User <User@MSI>
2023-03-17 15:19:45 +0800

    ITSA_10 輾轉相除法

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 int main()
 {
-	cout >> "hello world" >> endl;
+	int n1, n2;
 
+	cin >> n1 >> n2;
+	int n;
+	
+	while (n1 > 0 and n2 > 0)
+	{
+		if (n1 > n2)
+		{
+			n1 = n1 % n2;
+		}
+		else if (n2 > n1)
+		{
+			n2 = n2 % n1;
+		}
+	}
+	if (n1 > n2)
+	{
+		cout << n1 << endl;
+	}
+	else if (n2 > n1)
+	{
+		cout << n2 << endl;
+	}
 }
 
 
