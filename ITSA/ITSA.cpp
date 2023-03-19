0a9cdc060cab3ecb6843dcb90aea9d3f904ef9b4
User <User@MSI>
2023-03-19 19:57:58 +0800

    ITSA_14 判斷是否為迴文

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-	int n, nx = 0;
-	cin >> n;
-	int tn = n;
-	while (tn)
+	int main()
 	{
-		nx = nx * 10 + tn % 10;
-		tn /= 10;
+		int n, nx = 0;
+		cin >> n;
+		int tn = n;
+		while (tn)
+		{
+			nx = nx * 10 + tn % 10;
+			tn /= 10;
+		}
+		cout << (n == nx ? "YES" : "NO") << endl;
+		return 0;
 	}
-	cout << (n == nx?"YES":"NO");
-	return 0;
-}
 
 
